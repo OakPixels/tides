ae8f93facc9130a29fff286e78a3c9c2dc2ee844
JoeCornish <g6doug@gmail.com>
2020-02-27 11:26:24 +0000

    CORS

--- deploy.py
+++ deploy.py
@@ -3,8 +3,10 @@ from flask import Flask, request
 # Tides
 import urllib3
 from bs4 import BeautifulSoup
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def tides():
